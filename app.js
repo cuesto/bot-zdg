477b4dee43ce720b2de79b2241a98e5b30fb989b
Jonathan Cuesto <34cuesto@gmail.com>
2022-11-11 14:48:07 -0400

    update  whatsappjs version

--- app.js
+++ app.js
@@ -133,18 +133,18 @@ app.post('/send-media', async (req, res) => {
   const caption = req.body.caption;
   const fileUrl = req.body.file;
 
-  // const media = MessageMedia.fromFilePath('./image-example.png');
-  // const file = req.files.file;
-  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
-  let mimetype;
-  const attachment = await axios.get(fileUrl, {
-    responseType: 'arraybuffer'
-  }).then(response => {
-    mimetype = response.headers['content-type'];
-    return response.data.toString('base64');
-  });
+  //const media = MessageMedia.fromFilePath('./image-example.png');
+  const file = req.files.file;
+  const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+  // let mimetype;
+  // const attachment = await axios.get(fileUrl, {
+  //   responseType: 'stream'
+  // }).then(response => {
+  //   mimetype = response.headers['content-type'];
+  //   return response.data.toString('base64');
+  // });
 
-  const media = new MessageMedia(mimetype, attachment, 'Media');
+  // const media = new MessageMedia(mimetype, attachment, 'Media');
 
   client.sendMessage(number, media, {
     caption: caption
