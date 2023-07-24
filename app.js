f4959679895100c98a438152e8dd2972b7f2e3b0
Jonathan Cuesto <34cuesto@gmail.com>
2023-07-24 11:48:25 -0400

    dockerfile worked

--- app.js
+++ app.js
@@ -41,16 +41,16 @@ const client = new Client({
     ),
   puppeteer: {
     headless: true,
-    // args: [
-    //   '--no-sandbox',
-    //   '--disable-setuid-sandbox',
-    //   '--disable-dev-shm-usage',
-    //   '--disable-accelerated-2d-canvas',
-    //   '--no-first-run',
-    //   '--no-zygote',
-    //   '--single-process', // <- this one doesn't works in Windows
-    //   '--disable-gpu'
-    // ]
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ]
   }
 });
 
